49cdca66817cdd3bdb091bc9fb0a30330d1e467b
BrandyIron <brandy.iron@gmail.com>
2013-06-29 00:24:12 +0900

    Added Common PDO Library

---
+++ lib/include/nukotanDbh.php
@@ -0,0 +1,14 @@
+<?php
+
+function getPDO($env = null) {
+	$dsn = 'mysql:host=localhost;dbname=nukotan_live';
+	$username = 'nukotan';
+	$password = '0716';
+	$options = array(
+	PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+	);
+
+	$dbh = new PDO($dsn, $username, $password, $options);
+	return $dbh;
+}
+?>
